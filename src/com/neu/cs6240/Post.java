6e856b542ad07dc754fa6f3e6b8b8cec8cc982eb
vishrutshah <vishrut_n_shah@yahoo.com>
2014-11-21 18:25:23 -0500

    Converting Posts.xml to csv file using MapReduce

---
+++ src/com/neu/cs6240/Post.java
@@ -0,0 +1,160 @@
+package com.neu.cs6240;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+/**
+ * Post implements the Writable
+ */
+public class Post implements Writable {
+	// Post Id
+	Text id;
+	// PostTypeId; Question = 1, Answer = 2
+	Text postTypeId;
+	// Ans Id that has been Accepted as correct answer
+	Text acceptedAnswerId;
+	// Creation Date for this post whether its a question / answer
+	Text creationDate;
+	// User id who owns this Post
+	Text ownerUserId;
+	// Name of the user who last edited this post
+	Text lastEditorDisplayName;
+	// Date on which user who last edited this post
+	Text lastEditDate;
+	// last activity date on this post
+	Text lastActivityDate;
+	// All hash tags associated with this Post
+	Text tags;
+	// Number of answers given to this post if question
+	Text answerCount;
+	// CommentCount
+	Text commentCount;
+	// FavoriteCount
+	Text favoriteCount;
+	// CommunityOwnedDate
+	Text communityOwnedDate;
+	// title of this post is question
+	Text title;
+	// Score of this post
+	Text score;
+	// count as views of this posts
+	Text viewCount;
+	// Content of this post, sanitized
+	Text body;
+
+	/**
+	 * constructor
+	 */
+	public Post(Document doc) {
+		NodeList nList = doc.getElementsByTagName("row");
+		Element eElement = (Element) nList.item(0);
+		this.id = new Text(eElement.getAttribute("Id"));
+		this.postTypeId = new Text(eElement.getAttribute("PostTypeId"));
+		this.acceptedAnswerId = new Text(eElement.getAttribute("AcceptedAnswerId"));
+		this.creationDate = new Text(eElement.getAttribute("CreationDate"));
+		this.score = new Text(eElement.getAttribute("Score"));
+		this.viewCount = new Text(eElement.getAttribute("ViewCount"));
+		this.ownerUserId = new Text(eElement.getAttribute("OwnerUserId"));
+		this.lastEditorDisplayName = new Text(eElement.getAttribute("LastEditorDisplayName"));
+		this.lastEditDate = new Text(eElement.getAttribute("LastEditDate"));
+		this.lastActivityDate = new Text(eElement.getAttribute("LastActivityDate"));
+		this.tags = new Text(eElement.getAttribute("Tags"));
+		this.answerCount = new Text(eElement.getAttribute("AnswerCount"));
+		this.commentCount = new Text(eElement.getAttribute("CommentCount"));
+		this.favoriteCount = new Text(eElement.getAttribute("FavoriteCount"));
+		this.communityOwnedDate = new Text(eElement.getAttribute("CommunityOwnedDate"));
+		this.title = new Text(sanitize(eElement.getAttribute("Title")));
+		this.body = new Text(sanitize(eElement.getAttribute("Body")));
+	}
+	
+	/**
+	 * @return the id
+	 */
+	public Text getId() {
+		return id;
+	}
+
+	/**
+	 * overrider the write method to support write operation
+	 */
+	public void write(DataOutput out) throws IOException {
+		this.id.write(out);
+		this.postTypeId.write(out);
+		this.acceptedAnswerId.write(out);
+		this.creationDate.write(out);
+		this.score.write(out);
+		this.viewCount.write(out);
+		this.ownerUserId.write(out);
+		this.lastEditorDisplayName.write(out);
+		this.lastEditDate.write(out);
+		this.lastActivityDate.write(out);
+		this.tags.write(out);
+		this.answerCount.write(out);
+		this.commentCount.write(out);
+		this.favoriteCount.write(out);
+		this.communityOwnedDate.write(out);
+		this.title.write(out);
+		this.body.write(out);
+	}
+
+	/**
+	 * overrider readFiled method to support reading fields
+	 */
+	public void readFields(DataInput in) throws IOException {
+		this.id.readFields(in);
+		this.postTypeId.readFields(in);
+		this.acceptedAnswerId.readFields(in);
+		this.creationDate.readFields(in);
+		this.score.readFields(in);
+		this.viewCount.readFields(in);
+		this.ownerUserId.readFields(in);
+		this.lastEditorDisplayName.readFields(in);
+		this.lastEditDate.readFields(in);
+		this.lastActivityDate.readFields(in);
+		this.tags.readFields(in);
+		this.answerCount.readFields(in);
+		this.commentCount.readFields(in);
+		this.favoriteCount.readFields(in);
+		this.communityOwnedDate.readFields(in);
+		this.title.readFields(in);
+		this.body.readFields(in);
+	}
+	
+	private String sanitize(String data){
+		if(data == null || data.isEmpty()){
+			return data;
+		}
+		return data.replaceAll("(\\r|\\n|\\r\\n|,)+", "");
+	}
+	
+	public String toCsv(){
+		StringBuilder output = new StringBuilder();
+		
+		output.append(this.id.toString()).append(",");
+		output.append(this.postTypeId.toString()).append(",");
+		output.append(this.acceptedAnswerId.toString()).append(",");
+		output.append(this.creationDate.toString()).append(",");
+		output.append(this.score.toString()).append(",");
+		output.append(this.viewCount.toString()).append(",");
+		output.append(this.ownerUserId.toString()).append(",");		
+		output.append(this.lastEditorDisplayName.toString()).append(",");
+		output.append(this.lastEditDate.toString()).append(",");
+		output.append(this.lastActivityDate.toString()).append(",");
+		output.append(this.tags.toString()).append(",");
+		output.append(this.answerCount.toString()).append(",");
+		output.append(this.commentCount.toString()).append(",");
+		output.append(this.favoriteCount.toString()).append(",");		
+		output.append(this.communityOwnedDate.toString()).append(",");
+		output.append(this.title.toString()).append(",");
+		output.append(this.body.toString());
+		
+		return output.toString();
+	}
+}
---
+++ src/com/neu/cs6240/Xml2csvPosts.java
@@ -0,0 +1,147 @@
+/**
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.neu.cs6240;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.mahout.text.wikipedia.XmlInputFormat;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ * This class provides functionality to transform stack-overflow data dump's xml
+ * to csv format using Apache Hadoop
+ * 
+ * @author vishrut
+ * 
+ */
+public class Xml2csvPosts {
+
+	// Mapper class that reads line as a individual row
+	public static class PostsMapper extends
+			Mapper<LongWritable, Text, IntWritable, Text> {
+
+		private DocumentBuilderFactory factory = null;
+		private DocumentBuilder builder = null;
+		private Document doc = null;
+
+		/**
+		 * setup will be called once per Map Task before any of Map function
+		 * call, we'll initialize XML parser here
+		 */
+		protected void setup(Context context) {
+			this.factory = DocumentBuilderFactory.newInstance();
+			try {
+				this.builder = factory.newDocumentBuilder();
+			} catch (ParserConfigurationException e) {
+				// ParserConfigurationException => exit
+				System.exit(1);
+			}
+		}
+
+		public void map(LongWritable offset, Text value, Context context)
+				throws IOException, InterruptedException {
+
+			InputSource is = new InputSource(new StringReader(value.toString()));
+
+			try {
+				this.doc = this.builder.parse(is);
+			} catch (SAXException e) {
+				// Unable to parse => ignore row
+				return;
+			}
+
+			// parse xml row and emit only required fields
+			Post postObj = new Post(this.doc);
+
+			IntWritable key = new IntWritable();
+			key.set(Integer.parseInt(postObj.getId().toString()));
+
+			context.write(key, new Text(postObj.toCsv()));
+		}
+	}
+
+	// writes the data on the HDFS
+	public static class PostsReducer extends
+			Reducer<IntWritable, Text, NullWritable, Text> {
+
+		public void reduce(IntWritable key, Iterable<Text> values,
+				Context context) throws IOException, InterruptedException {
+			for (Text value : values) {
+				context.write(NullWritable.get(), value);
+			}
+		}
+	}
+
+	// Paritioner for scalability
+	public static class PostsPartitioner extends
+			Partitioner<IntWritable, Text> {
+		/**
+		 * Based on the configured number of reducer, this will partition the
+		 * data approximately evenly based on number of unique post ids
+		 */
+		@Override
+		public int getPartition(IntWritable key, Text value, int numPartitions) {
+			// multiply by 127 to perform some mixing
+			return Math.abs(key.hashCode() * 127) % numPartitions;
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		// Setting up the xml tag configurator for splitter
+		conf.set("xmlinput.start", "<row ");
+		conf.set("xmlinput.end", " />");
+
+		String[] otherArgs = new GenericOptionsParser(conf, args)
+				.getRemainingArgs();
+		if (otherArgs.length != 2) {
+			System.err.println("Usage: Xml2csvPosts <in> <out>");
+			System.exit(2);
+		}
+		Job job = new Job(conf, "Converts Posts.xml to .csv");
+		job.setJarByClass(Xml2csvPosts.class);
+		job.setInputFormatClass(XmlInputFormat.class);
+		job.setMapperClass(PostsMapper.class);
+		job.setReducerClass(PostsReducer.class);
+		job.setPartitionerClass(PostsPartitioner.class);
+		job.setOutputKeyClass(IntWritable.class);
+		job.setOutputValueClass(Text.class);
+		// Set as per your file size
+		job.setNumReduceTasks(10);
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
